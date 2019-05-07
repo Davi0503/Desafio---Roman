d47a88ccfbacf8158e5ea09548072056239af324
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2019-05-07 08:38:58 -0300

    Update DB_ValoresIniciais.sql

--- banco de dados/DB_ValoresIniciais.sql
+++ banco de dados/DB_ValoresIniciais.sql
@@ -1,20 +1,12 @@
--- Scripts de inserÁ„o de usuarios
-/*
-Valores para equipes:
-1 = Administrador
-2 = Desenvolvimento
-3 = Redes
-4 = Multimidia
-Qualquer outro valor n„o ser· aceito (CONSTRAINT EQUIPEVALIDA)
-*/
+-- Scripts de inser√ß√£o de usuarios
 INSERT INTO USUARIOS(EMAIL,SENHA,EQUIPE) VALUES
-('admin@admin.com','123456778',1),
-('helena@gmail.com','umasenhadificil123',2),
-('professorderedesqueeunaoseionome@gmail.com','umasenhamaisdificilqueaanterior123',3),
-('email@gmail.com','senha@senha',4);
+('admin@admin.com','123456778','Administrador'),
+('helena@gmail.com','umasenhadificil123','Desenvolvimento'),
+('professorderedesqueeunaoseionome@gmail.com','umasenhamaisdificilqueaanterior123','Redes'),
+('email@gmail.com','senha@senha','Multimidia');
 
--- Script de inserÁ„o de temas
-INSERT INTO TEMAS(NOME) VALUES ('Gest„o'),('HQs')
+-- Script de inser√ß√£o de temas
+INSERT INTO TEMAS(NOME) VALUES ('Gest√£o'),('HQs')
 
--- Script de inserÁ„o de Projetos (deve deferenciar um tema existente)
+-- Script de inser√ß√£o de Projetos (deve deferenciar um tema existente)
 INSERT INTO PROJETOS(NOME,IDTEMA) VALUES('Controle de Estoque',1),('Listagem de personagens',2);
