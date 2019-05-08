0023c885416242c9d42370622005cb9808b859aa
Kevin Benevides <kevinhobene@gmail.com>
2019-05-08 17:43:35 -0300

    Estilizando pagina login

--- roman/src/pages/login.js
+++ roman/src/pages/login.js
@@ -7,13 +7,18 @@ import {
     ImageBackground,
     TextInput,
     TouchableOpacity,
-    AsyncStorage
+    AsyncStorage,
+    ScrollView
 } from "react-native";
 import api from '../services/api.js';
 
 
 
 class Login extends Component {
+    static navigationOptions = {
+        header: null
+    };
+
     constructor(props) {
         super(props);
         this.state = { email: "", senha: "" };
@@ -27,7 +32,7 @@ class Login extends Component {
             email: this.state.email,
             senha: this.state.senha
         })
-        
+
         const token = respostaLogin.data.token;
         await AsyncStorage.setItem('userToken', token);
         this.props.navigation.navigate("MainNavigator");
@@ -37,31 +42,65 @@ class Login extends Component {
 
     render() {
         return (
-            <View>
-                <View>
-                    <Text>Login</Text>
-                </View>
-                <View>
-                    <TextInput
-                        placeholder="email"
-                        textContentType='emailAddress'
-                        onChangeText={email => this.setState({ email })}
-                    />
-                    <TextInput
-                        placeholder="senha"
-                        textContentType='password'
-                        onChangeText={senha => this.setState({ senha })}
-                    />
 
-                    <TouchableOpacity
-                        onPress={this._logando}
-                    >
-                        <Text>Login</Text>
-                    </TouchableOpacity>
-                </View>
+            <View style={styles.main}>
+                {/* <View style={styles.container}> */}
+                {/* </View> */}
+
+                <ImageBackground source={require("../assets/img/160.jpg")} style={styles.backgroundImage}>
+                    <View style={styles.mainContainer}>
+
+                        <View>
+                            <Text>Login</Text>
+                        </View>
+                        <View>
+                            <Text>Email:</Text>
+                            <TextInput
+                                placeholder="email"
+                                textContentType='emailAddress'
+                                onChangeText={email => this.setState({ email })}
+                            />
+                            <Text>Senha:</Text>
+                            <TextInput
+                                placeholder="senha"
+                                textContentType='password'
+                                onChangeText={senha => this.setState({ senha })}
+                            />
+
+                            <TouchableOpacity
+                                onPress={this._logando}
+                            >
+                                <Text>Login</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                </ImageBackground>
+
             </View>
         )
     }
+
 }
+
+const styles = StyleSheet.create({
+    main: {
+        padding: 0,
+        margin: 0,
+        display:"flex"
+    },
+    mainContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#000',
+        
+    },
+    backgroundImage: {
+        width: "100%", height: 650, resizeMode: 'cover'
+
+    }
+
+})
 
 export default Login
--- roman/src/pages/main.js
+++ roman/src/pages/main.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react"
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text } from "react-native"
 
 class Main extends Component {
-    render() {
-        return (
+    render(){
+        return(
             <View>
                 <Text>
                     Projetos
                 </Text>
+                <View>
+                    <Link to="/cadastroProjetos">+</Link>
+                </View>
+                <View>
+                    <FlatList
+                    // contentContainerStyle={}
+                    data={this.state.listaprojetos}
+                    keyExtractor={item => item.id}
+                    renderItem={this.renderizaItem}
+                    />
+                </View>
             </View>
         )
     }
+
+    renderizaItem = ({item}) => (
+        <View>
+            <View>
+                <View>
+                    <Text>{item.nome}</Text>
+                    <Text>{item.tema}</Text>
+                    <Text>{item.ativo}</Text>
+                    <Link to="/atualiza">Atualizar</Link>
+                </View>
+            </View>
+        </View>
+    )
 }
 
 export default Main
